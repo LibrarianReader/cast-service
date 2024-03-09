cdbb9830e212e39a42d4345da9c89bbab02bbebd
LibrarianReader <mail81025@mail.ru>
2024-03-09 17:05:09 +0300

    21 run tests

--- cast_service_test.py
+++ cast_service_test.py
@@ -1,8 +1,30 @@
 from fastapi.testclient import TestClient
 from app.main import app
+from app.api.models import CastIn, CastOut, CastUpdate
+
 
 client = TestClient(app)
 
+cast_data = {
+    'name': 'Name',
+    'nationality': 'Country',
+}
+
+def test_create_cast():
+    cast = CastIn(**cast_data)
+    assert dict(cast) == cast_data
+
+def test_update_cast_name():
+    cast = CastIn(**cast_data)
+    cast_upd = CastUpdate(name='New Name', **cast_data)
+    assert dict(cast_upd) == {'name': 'New Name', 'nationality': 'Country'}
+
+def test_update_cast_nationality():
+    cast = CastIn(**cast_data)
+    cast_upd = CastUpdate(nationality='New Country', **cast_data)
+    assert dict(cast_upd) == {'name': 'Name', 'nationality': 'New Country'}
+
+'''
 def create_cast_success():
     data = {
         "name": "string",
@@ -25,3 +47,4 @@ def test_get_cast_success():
 def test_get_cast_failure():
     response = client.get("/api/v1/casts/999/", headers={"accept": "application/json"})
     assert response.status_code == 404
+'''
