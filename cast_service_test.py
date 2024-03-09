221e6ad46ae3ddb8f11be3936ac8c34a641cc394
LibrarianReader <mail81025@mail.ru>
2024-03-09 17:06:44 +0300

    22 run tests

--- cast_service_test.py
+++ cast_service_test.py
@@ -23,28 +23,3 @@ def test_update_cast_nationality():
     cast = CastIn(**cast_data)
     cast_upd = CastUpdate(nationality='New Country', **cast_data)
     assert dict(cast_upd) == {'name': 'Name', 'nationality': 'New Country'}
-
-'''
-def create_cast_success():
-    data = {
-        "name": "string",
-        "nationality": "string"
-    }
-    response = client.post("/api/v1/casts/", json=data, headers={"accept": "application/json", "Content-Type": "application/json"})
-    assert response.status_code == 200
-    assert response.headers["content-type"] == "application/json"
-
-def create_cast_failure():
-    data = {}
-    response = client.post("/api/v1/casts/", json=data, headers={"accept": "application/json", "Content-Type": "application/json"})
-    assert response.status_code == 422
-
-def test_get_cast_success():
-    response = client.get("/api/v1/casts/1/", headers={"accept": "application/json"})
-    assert response.status_code == 200
-    assert response.headers["content-type"] == "application/json"
-
-def test_get_cast_failure():
-    response = client.get("/api/v1/casts/999/", headers={"accept": "application/json"})
-    assert response.status_code == 404
-'''
